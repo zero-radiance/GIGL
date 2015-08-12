546f4a114be2236ef078dea37c7b50bc3e6ca6b1
Evgenii Golubev <golubev@stud.uni-saarland.de>
2015-08-12 14:19:17 +0200

    GLUniformManager now has a default constructor. setManagedUniforms() is responsible for initialization. Also, updated the names of the function parameters to make them more specific.

--- Source/GL/GLUniformManager.hpp
+++ Source/GL/GLUniformManager.hpp
@@ -3,33 +3,39 @@
 
 template <int N>
 GLUniformManager<N>::GLUniformManager(const GLSLProgram& sp,
-                                      std::initializer_list<const char*> args) {
+                                      std::initializer_list<const char*> names) {
+    setManagedUniforms(sp, names);
+}
+
+template <int N>
+void GLUniformManager<N>::setManagedUniforms(const GLSLProgram& sp,
+                                             std::initializer_list<const char*> names) {
     #if __cplusplus <= 201103L
         // No constexpr support
-        assert(args.size() == N);
+        assert(names.size() == N);
     #else
-        static_assert(args.size() == N, "Mismatch between the number of uniforms N and "
+        static_assert(names.size() == N, "Mismatch between the number of uniforms N and "
                                         "the number of function arguments.");
     #endif
     // Query for locations
     int i = 0;
-    for (const auto arg : args) {
-        m_uni_locs[i++] = sp.getUniformLocation(arg);
+    for (const auto name : names) {
+        m_uni_locs[i++] = sp.getUniformLocation(name);
     }
 }
 
 template <int N>
 template <typename... Args>
-void GLUniformManager<N>::setUniformValues(const Args&... args) const {
-    setUniforms<0>(args...);
+void GLUniformManager<N>::setUniformValues(const Args&... vals) const {
+    setUniforms<0>(vals...);
 }
 
 template <int N>
 template <int I, typename T, typename... Args>
-void GLUniformManager<N>::setUniforms(const T& first, const Args&... args) const {
+void GLUniformManager<N>::setUniforms(const T& first, const Args&... vals) const {
     static_assert(I <= N - 1, "Invalid number of function arguments.");
     GLSLProgram::setUniformValue(m_uni_locs[I], first);
-    setUniforms<I + 1>(args...);
+    setUniforms<I + 1>(vals...);
 }
 
 template <int N>
